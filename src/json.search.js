e78b413c2f3c23704145f5a03d2a79250741cb7d
Hakan Bilgin <hbi99@hotmail.com>
2014-07-29 22:11:19 +0200

    fixes

--- src/json.search.js
+++ src/json.search.js
@@ -4,91 +4,30 @@ if (!JSON.search) {
 		'use strict';
 		
 		var doc  = JSON.toXML(tree),
-			od   = doc.documentElement,
 			xres = Defiant.node[ single ? 'selectSingleNode' : 'selectNodes' ](doc, xpath),
-			jres = [],
+			i    = xres.length,
 			ret  = [],
-			map,
-			node,
-			map_index,
-			item_map,
-			current,
-			is_attr,
-			key,
-			do_search = function(current) {
-				var j, jl, check;
-				if (map_index === jres[i].length) return current;
-				switch (current.constructor) {
-					case Array:
-						for (j=0, jl=current.length; j<jl; j++) {
-							if (item_map.val === JSON.stringify(current[j], null, '\t')) {
-								if (map_index < jres[i].length) {
-									map_index++;
-									item_map = jres[i][map_index];
-									return do_search( current[j] );
-								}
-							}
-						}
-						if (j === jl) {
-							if (item_map.val === JSON.stringify(current, null, '\t')) {
-								map_index++;
-								item_map = jres[i][map_index];
-
-								for (j=0; j<jl; j++) {
-									check = do_search( current[j] );
-									if (check) return check;
-								}
-							}
-							return current;
-						}
-						break;
-					default:
-						is_attr = jres[i][map_index].node.nodeType;
-						key = (is_attr === 1)? jres[i][map_index].node.getAttribute('d:name') : false;
-						//key = jres[i][map_index].node.getAttribute('d:name');
-						key = key || item_map.key;
-						current = current[key];
-						if (typeof(current) !== 'object') {
-							map_index++;
-							item_map = jres[i][map_index];
-							return current;
-						}
-						if (current.constructor === Object) {
-							map_index++;
-							item_map = jres[i][map_index];
-						}
-				}
-				return do_search( current );
-			};
+			mapIndex;
 
 		if (single) xres = [xres];
+console.log( this.search );
 		//console.log( 'x-RES:', xres );
-		for (i=0, il=xres.length; i<il; i++) {
-			map  = [];
-			node = xres[i];
-			// can't access json "root"
-			if (node === od) continue;
-			while (node !== od) {
-				is_attr = node.nodeType === 2;
-				map.push({
-					node : node,
-					key  : (is_attr ? '@' : '') + node.nodeName,
-					val  : is_attr ? node.value : Defiant.node.toJSON(node, '\t')
-				});
-				node = is_attr ? node.ownerElement : node.parentNode;
+		while (i--) {
+			switch(xres[i].nodeType) {
+				case 2:
+				case 3: 
+					ret.unshift( xres[i].nodeValue );
+					break;
+				default:
+					mapIndex = +xres[i].getAttribute('d:mi');
+					ret.unshift( this.search.map[mapIndex-1] );
 			}
-			jres.push(map.reverse());
-		}
-		//console.log( 'j-RES:', jres );
-		for (var i=0, il=jres.length; i<il; i++) {
-			map_index  = 0;
-			item_map   = jres[i][map_index];
-			ret.push( do_search(tree) );
 		}
 		// if tracing is enabled
-		this.trace = JSON.search.trace ? JSON.mtrace(tree, jres) : false;
+		if (typeof JSON.mtrace === 'function') JSON.mtrace();
+		//this.trace = JSON.search.trace ? JSON.mtrace(tree, ret) : false;
 
 		//console.log( 'RES:', ret );
-		return Defiant.result(ret);
+		return ret;
 	};
 }

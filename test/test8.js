06b2184bf77a433353511eb92c50db4103fafb6f
Hakan Bilgin <hbi99@hotmail.com>
2015-01-10 12:18:07 +0100

    added new test
    …and grunted version

--- test/test8.js
+++ test/test8.js
@@ -1,3 +1,13 @@
 
 "use strict";
 
+describe('String manipulation', function() {
+	var str1 = "//*[contains(title, 'rings')]/title",
+		str2 = str1.xTransform();
+
+	it('test completed.', function() {
+		expect( str2 ).toEqual( "//*[contains(translate(title, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'rings')]/title" );
+	});
+});
+
+

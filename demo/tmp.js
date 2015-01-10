2570e452684991ec3d752a135190a4fa996e4d94
Hakan Bilgin <hbi99@hotmail.com>
2015-01-10 11:55:53 +0100

    adjustments

--- demo/tmp.js
+++ demo/tmp.js
@@ -1,37 +1,37 @@
 test.init({
-   "store": {
-      "book": [
-         {
-            "@price": 12.99,
-            "title": "Sword of Honour",
-            "category": "fiction",
-            "author": "Evelyn Waugh"
-         },
-         {
-            "@price": 8.99,
-            "title": "Moby Dick",
-            "category": "fiction",
-            "author": "Herman Melville",
-            "isbn": "0-553-21311-3"
-         },
-         {
-            "@price": 8.95,
-            "title": "Sayings of the Century",
-            "category": "reference",
-            "author": "Nigel Rees"
-         },
-         {
-            "@price": 22.99,
-            "title": "The Lord of the Rings",
-            "category": "fiction",
-            "author": "J. R. R. Tolkien",
-            "isbn": "0-395-19395-8"
-         }
-      ],
-      "bicycle": {
-         "@price": 19.95,
-         "brand": "Cannondale",
-         "color": "red"
-      }
-   }
+	"store": {
+		"book": [
+			{
+				"@price": 12.99,
+				"title": "Sword of Honour",
+				"category": "fiction",
+				"author": "Evelyn Waugh"
+			},
+			{
+				"@price": 8.99,
+				"title": "Moby Dick",
+				"category": "fiction",
+				"author": "Herman Melville",
+				"isbn": "0-553-21311-3"
+			},
+			{
+				"@price": 8.95,
+				"title": "Sayings of the Century",
+				"category": "reference",
+				"author": "Nigel Rees"
+			},
+			{
+				"@price": 22.99,
+				"title": "The Lord of the Rings",
+				"category": "fiction",
+				"author": "J. R. R. Tolkien",
+				"isbn": "0-395-19395-8"
+			}
+		],
+		"bicycle": {
+			"@price": 19.95,
+			"brand": "Cannondale",
+			"color": "red"
+		}
+	}
 });
